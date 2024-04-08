62f22d937b0bea1c5fbaac5d1e5da1678f1ac090
EL-Suescun <santiagogiraldosuescun@gmail.com>
2024-04-07 22:10:33 -0500

    Creamos Spring Boot
    Creamos los archivos de Spring Boot

---
+++ pqrds/pqrds/src/main/java/co/com/personal/api/pqrds/PqrdsApplication.java
@@ -0,0 +1,13 @@
+package co.com.personal.api.pqrds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PqrdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PqrdsApplication.class, args);
+	}
+
+}
---
+++ pqrds/pqrds/src/test/java/co/com/personal/api/pqrds/PqrdsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.personal.api.pqrds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PqrdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
